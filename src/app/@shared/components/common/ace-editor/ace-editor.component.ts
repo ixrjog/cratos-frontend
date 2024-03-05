4acd5b4a866ef5c7a4d7dcaf15d691b6ad7c9f1e
修远 <448613222@qq.com>
2024-03-05 14:35:11 +0800

    ace  增强

--- src/app/@shared/components/common/ace-editor/ace-editor.component.ts
+++ src/app/@shared/components/common/ace-editor/ace-editor.component.ts
@@ -9,6 +9,7 @@ import * as ace from 'ace-builds';
 export class AceEditorComponent implements AfterViewInit {
   /**
    * @see <a href="https://ace.c9.io/#nav=howto">ace 文档</a>
+   * @see <a href="https://github.com/ajaxorg/ace/wiki/Configuring-Ace">ace config</a>
    * @see <a href="https://jfcere.github.io/ngx-markdown/get-started">ngx-markdown 文档</a>
    */
   @ViewChild('editor') private editor: ElementRef<HTMLElement>;
@@ -16,19 +17,27 @@ export class AceEditorComponent implements AfterViewInit {
   @Input() readonly: boolean = false;
   @Input() minLines: number = 5;
   @Input() maxLines: number = 50 ;
+  @Input() showLineNumbers: boolean = true;
+
   /**
    * @see <a href="https://github.com/ajaxorg/ace/tree/master/src/theme">theme 列表</a>
    */
-  @Input() theme: string = 'ace/theme/chrome';
+  @Input() theme: string = 'ace/theme/tomorrow';
   /**
    * @see <a href="https://github.com/ajaxorg/ace/tree/master/src/mode">mode 列表</a>
    */
   @Input() mode: string = 'ace/mode/markdown';
 
+  options = {
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true,
+    enableSnippets: true,
+  };
+
   @Output() onChange = new EventEmitter<string>();
 
+
   ngAfterViewInit(): void {
-    ace.config.set('basePath', 'https://unpkg.com/ace-builds@1.4.12/src-noconflict');
     let fontSize: string;
     switch (localStorage.getItem('font')) {
       case 'normal':
@@ -47,7 +56,10 @@ export class AceEditorComponent implements AfterViewInit {
     ace.config.set('fontSize', fontSize);
     ace.config.set('minLines', this.minLines);
     ace.config.set('maxLines', this.maxLines);
+    ace.config.set('showLineNumbers', this.showLineNumbers);
     const aceEditor = ace.edit(this.editor.nativeElement);
+    ace.require('ace/ext/language_tools');
+    aceEditor.setOptions(this.options);
     aceEditor.setReadOnly(this.readonly);
     aceEditor.session.setValue(this.aceValue);
     aceEditor.setTheme(this.theme);

4a57fc8e7c2ac539eba51ecee34808624cbf9ef8
修远 <448613222@qq.com>
2024-03-11 16:36:47 +0800

    commit

--- src/app/pages/menu-util.ts
+++ src/app/pages/menu-util.ts
@@ -1,18 +1,6 @@
 export function getMenu(values: any): any[] {
   return [
     {
-      title: values['gettingStarted']['title'],
-      open: false,
-      children: [
-        {
-          title: values['gettingStarted']['sample'],
-          link: '/pages/getting-started/sample',
-        },
-      ],
-      link: '/pages/getting-started',
-      menuIcon: 'icon icon-console',
-    },
-    {
       title: values['sys']['title'],
       open: true,
       children: [

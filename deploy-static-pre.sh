2459a80e020c694d2636981ce7791e6b661ae55b
白衣 <ixrjog@qq.com>
2025-03-14 11:18:27 +0800

    daily

---
+++ deploy-static-pre.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. /etc/profile
+
+nvm use v18.19.0
+npm run build:prod
+
+echo "Push to OSS."
+# deploy to OSS
+ossutil -c ~/.ossutilconfig cp -f -u --recursive ./dist/ oss://opscloud4-web-hz/cratos-pre/

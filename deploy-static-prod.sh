c77fbcf7718756c56ccad8a949ba722fea8b26ca
修远 <448613222@qq.com>
2025-04-17 16:07:25 +0800

    fix

--- deploy-static-prod.sh
+++ deploy-static-prod.sh
@@ -5,6 +5,10 @@
 nvm use v18.19.0
 npm run build:prod
 
-echo "Push to OSS."
+echo "Push to pre OSS."
+# deploy to OSS
+ossutil -c ~/.ossutilconfig cp -f -u --recursive ./dist/ oss://opscloud4-web-hz/cratos-pre/
+
+echo "Push to prod OSS."
 # deploy to OSS
 ossutil -c ~/.ossutilconfig cp -f -u --recursive ./dist/ oss://opscloud4-web-hz/cratos-prod/

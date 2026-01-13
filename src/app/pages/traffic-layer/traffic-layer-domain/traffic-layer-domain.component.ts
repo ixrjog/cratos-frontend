a8730c63bf6dc13afe9cd15b31b9d4e852e43789
修远 <448613222@qq.com>
2026-01-13 15:32:11 +0800

    update traffic layer domain component

--- src/app/pages/traffic-layer/traffic-layer-domain/traffic-layer-domain.component.ts
+++ src/app/pages/traffic-layer/traffic-layer-domain/traffic-layer-domain.component.ts
@@ -1,7 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
 import {
-  TrafficLayerDomainDataTableComponent
+  TrafficLayerDomainDataTableComponent,
 } from './traffic-layer-domain-data-table/traffic-layer-domain-data-table.component';
+import {
+  TrafficLayerRecordDataTableComponent,
+} from './traffic-layer-record-data-table/traffic-layer-record-data-table.component';
 
 @Component({
   selector: 'app-traffic-layer-domain',
@@ -10,7 +13,23 @@ import {
 })
 export class TrafficLayerDomainComponent {
   @ViewChild('domainDataTable') private domainDataTable: TrafficLayerDomainDataTableComponent;
+  @ViewChild('recordDataTable') private recordDataTable: TrafficLayerRecordDataTableComponent;
+
+  tabActiveId: string | number = 'domain';
 
   constructor() {
+  }
+
+  onActiveTabChange(tab) {
+    switch (tab) {
+      case 'domain':
+        this.domainDataTable.fetchData();
+        break;
+      case 'record':
+        this.recordDataTable.fetchData();
+        break;
+      default:
+        break;
+    }
   }
 }

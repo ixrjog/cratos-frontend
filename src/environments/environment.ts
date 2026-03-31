86d1acc5eb3614433fc17fbabc66251ab74f437f
修远 <jian.xu1@palmpay-inc.com>
2026-03-31 16:15:44 +0800

    chore: switch proxy and ws config to prod environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  //wsUrl: 'wss://cratos.palmpay-inc.com',
-  wsUrl: 'ws://127.0.0.1:8081',
+  wsUrl: 'wss://cratos.palmpay-inc.com',
+  // wsUrl: 'ws://127.0.0.1:8081',
   production: false,
 };

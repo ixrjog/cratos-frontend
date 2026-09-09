4c052053266f6e929787088d3f0677c98841287a
白衣 <ixrjog@qq.com>
2026-09-09 10:16:43 +0800

    chore(deploy): drop pre OSS push, keep prod only

--- deploy-static-prod.sh
+++ deploy-static-prod.sh
@@ -5,10 +5,6 @@ source ~/.nvm/nvm.sh
 nvm use v18.19.0
 npm run build:prod
 
-echo "Push to pre OSS."
-# deploy to OSS
-ossutil -c ~/.ossutilconfig cp -f -u --recursive ./dist/ oss://opscloud4-web-hz/cratos-pre/
-
 echo "Push to prod OSS."
 # deploy to OSS
 ossutil -c ~/.ossutilconfig cp -f -u --recursive ./dist/ oss://opscloud4-web-hz/cratos-prod/
